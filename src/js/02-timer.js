b7f7070f618276f4193a3bf722a6d784e41e4ef7
Alyona-Nekhryshchyniuk <o.nekhryshchyniuk@gmail.com>
2022-12-04 22:41:33 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,35 +1,49 @@
 import AirDatepicker from 'air-datepicker';
 import 'air-datepicker/air-datepicker.css';
 
-// const startBut = document.querySelector('[data-start]');
+const daySpan = document.querySelector('[data-days]');
+const hourSpan = document.querySelector('[data-hours]');
+const minSpan = document.querySelector('[data-minutes]');
+const secSpan = document.querySelector('[data-seconds]');
+// const input = document.querySelector('#datetime-picker');
+const startBut = document.querySelector('[data-start]');
 
-const input = document.querySelector('#datetime-picker');
+let startTimerValue;
+
 const air = new AirDatepicker('#datetime-picker', {
   timepicker: true,
   onSelect(formattedDate) {
-    console.log(formattedDate.date);
+    let currentTime = new Date();
+    let chosen_Date_Time = formattedDate.date;
+    startTimerValue = chosen_Date_Time - currentTime;
+    console.log(startTimerValue);
+    let chosenDateObj = getTimeComponents(startTimerValue);
+    console.log(chosenDateObj);
+    daySpan.textContent = chosenDateObj.days;
+    hourSpan.textContent = chosenDateObj.hours;
+    minSpan.textContent = chosenDateObj.mins;
+    secSpan.textContent = chosenDateObj.secs;
   },
 });
-let z = air.onSelect;
-console.log(z);
-console.log(new Date());
+
 // input.addEventListener('focus', () => {
 //   console.log(air.viewDate);
 // });
-// const setTimer = () => {
-//   const subtrahend = Date.now();
+const startTimerOnClick = () => {
+  const subtrahend = Date.now();
 
-//   return setInterval(() => {
-//     const minuend = Date.now();
-//     console.log(getTimeComponents(minuend - subtrahend));
-//   }, 1000);
-// };
+  return setInterval(() => {
+    const minuend = Date.now();
+    console.log(getTimeComponents(minuend - subtrahend));
+  }, 1000);
+};
 
-// startBut.addEventListener('click', setTimer);
+startBut.addEventListener('click', startTimerOnClick);
 
 function getTimeComponents(time) {
+  const days = Math.floor(time / (1000 * 60 * 60 * 24));
   const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
   const secs = Math.floor((time % (1000 * 60)) / 1000);
-  return { hours, mins, secs };
+  return { days, hours, mins, secs };
 }
