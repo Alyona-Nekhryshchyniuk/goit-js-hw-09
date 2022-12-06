d3df4218a23a80ad5662a313fd0ee613598d8499
Alyona-Nekhryshchyniuk <o.nekhryshchyniuk@gmail.com>
2022-12-06 16:10:29 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,24 +1,26 @@
 const button = document.querySelector('button');
-// const delay = document.querySelector('[name="delay"]');
-const delay = form.elements.delay;
-console.log(delay);
-// const step = document.querySelector('[name="step"]');
-// const amount = document.querySelector('[name="amount"]');
+const delay = document.querySelector('[name="delay"]');
+const step = document.querySelector('[name="step"]');
+const amount = document.querySelector('[name="amount"]');
+const position = amount.value;
+console.log(position);
+// const delay
+// const firstDelay
 
-// button.addEventListener('click', regularPromiseCreation());
+const regularPromiseCreation = (position, delay, firstDelay) => {
+  setTimeout(createPromise(1, 0), firstDelay);
+  for (const i = position; i >= 1; i--) {
+    setTimeout(createPromise(position, delay), 1000);
+  }
+};
 
-// const regularPromiseCreation = (position, delay, firstDelay) => {
-//   setTimeout(createPromise(1, 0), firstDelay);
-//   for (const i = position; i >= 1; i--) {
-//     setTimeout(createPromise(position, delay), 1000);
-//   }
-// };
+button.addEventListener('click', regularPromiseCreation());
 
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//   } else {
-//     // Reject
-//   }
-// }
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  if (shouldResolve) {
+    // Fulfill
+  } else {
+    // Reject
+  }
+}
