5043f19f7ad6ec7ec1b12ba24ce5a62d73f42045
Alyona-Nekhryshchyniuk <o.nekhryshchyniuk@gmail.com>
2022-12-06 15:39:21 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,24 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+const button = document.querySelector('button');
+// const delay = document.querySelector('[name="delay"]');
+const delay = form.elements.delay;
+console.log(delay);
+// const step = document.querySelector('[name="step"]');
+// const amount = document.querySelector('[name="amount"]');
+
+// button.addEventListener('click', regularPromiseCreation());
+
+// const regularPromiseCreation = (position, delay, firstDelay) => {
+//   setTimeout(createPromise(1, 0), firstDelay);
+//   for (const i = position; i >= 1; i--) {
+//     setTimeout(createPromise(position, delay), 1000);
+//   }
+// };
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
