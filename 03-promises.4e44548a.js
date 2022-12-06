26fac4183a1d9cf7a476ed4534e809f0376cb9a9
Alyona-Nekhryshchyniuk <108234116+Alyona-Nekhryshchyniuk@users.noreply.github.com>
2022-12-06 13:41:56 +0000

    Deploying to gh-pages from @ Alyona-Nekhryshchyniuk/goit-js-hw-09@5043f19f7ad6ec7ec1b12ba24ce5a62d73f42045 🚀

---
+++ 03-promises.4e44548a.js
@@ -0,0 +1,2 @@
+document.querySelector("button");const e=form.elements.delay;console.log(e);
+//# sourceMappingURL=03-promises.4e44548a.js.map
---
+++ 03-promises.c039b9b1.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector("button");var e=form.elements.delay;console.log(e)}();
+//# sourceMappingURL=03-promises.c039b9b1.js.map
