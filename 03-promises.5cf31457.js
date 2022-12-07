68518cc132e06a294b8a560b3fbdb31dc475ffcb
Alyona-Nekhryshchyniuk <108234116+Alyona-Nekhryshchyniuk@users.noreply.github.com>
2022-12-07 17:27:06 +0000

    Deploying to gh-pages from @ Alyona-Nekhryshchyniuk/goit-js-hw-09@78a9112de554cf6c778d98a6030d80a512ecb518 🚀

--- 03-promises.5cf31457.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("button"),t=(document.querySelector('[name="delay"]'),document.querySelector('[name="step"]'),document.querySelector('[name="amount"]').value);console.log(t);function o(e,t){Math.random()}e.addEventListener("click",function(e,t,n){setTimeout(o(1,0),n);for(var u=e;u>=1;u--)setTimeout(o(e,t),1e3)}())}();
-//# sourceMappingURL=03-promises.5cf31457.js.map
--- 03-promises.fb1b3a39.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("button"),t=(document.querySelector('[name="delay"]'),document.querySelector('[name="step"]'),document.querySelector('[name="amount"]').value);console.log(t);function o(e,t){Math.random()}e.addEventListener("click",((e,t,n)=>{setTimeout(o(1,0),n);for(const n=e;n>=1;n--)setTimeout(o(e,t),1e3)})());
-//# sourceMappingURL=03-promises.fb1b3a39.js.map
